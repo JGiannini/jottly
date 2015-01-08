c1b60c4b3a9a600efde5e0cec3fe16780b6763ac
Jamie <jgianninidesign@gmail.com>
2015-01-07 20:41:55 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+function clicked(button){
+	alert("Good job!");
+	onclose(alert("Good job (x2)"));
+}
+
+
+function hoverOver(button){
+	button.innerHTML="Go ahead.";
+}
+
+function unHover(button){
+	button.innerHTML="Click on Me";
+}
